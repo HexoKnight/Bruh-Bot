5d30b768626281ab5c3606643fc038fe981e1504
Harvey-G <harvey.gream@gmail.com>
2023-02-08 18:22:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,27 +12,26 @@ TOKEN = "OTkxMjgxMzMzOTE5ODMwMDQ2.GCjxv3.bZweE0DTGyx2eSwDpyPYV9SrYEqK3HWZM8ZPMY"
 TheGroup_id = 761690744703942706
 bruhChannel_id = 991363080720220230
 Harvaria_id = 571981658874445836
-Lounge_id = 761690744703942712
+#Lounge_id = 761690744703942712
 intents = discord.Intents.all()
+
+server_ids = [TheGroup_id, 1059896150720786492] # samuel's server
+server_objects = [discord.Object(id) for id in server_ids]
 
 bruhUses = {}
 
 class myclient(discord.Client):
   def __init__(self):
     super().__init__(intents=intents)
-    self.synced = False
 
   async def on_ready(self):
     await self.wait_until_ready()
-    if not self.synced:
-      await tree.sync(guild = discord.Object(id = TheGroup_id))
-      self.synced = True
     print(f"Successfully logged in as {self.user}.")
 
 client = myclient()
 tree = app_commands.CommandTree(client)
 
-@tree.command(name = "bruh", description = "bruh", guild = discord.Object(TheGroup_id))
+@tree.command(name = "bruh", description = "bruh", guilds = server_objects)
 async def bruh(interaction: discord.Interaction, length: int, secret: bool = False):
   try:
     aclength = length
@@ -88,9 +87,14 @@ async def bruh(interaction: discord.Interaction, length: int, secret: bool = Fal
       channel = interaction.user.voice.channel
       go = True
     else:
-      channel = client.get_channel(Lounge_id)
-      if len(channel.voice_states) > 0:
+      voice_channels = interaction.guild.voice_channels
+      occupied_voice_channels = [voice_channel for voice_channel in voice_channels if len(voice_channel.members) > 0]
+      if len(occupied_voice_channels) > 0:
+        channel = occupied_voice_channels[0] # just use first if multiple
         go = True
+      #channel = client.get_channel(Lounge_id)
+      #if len(channel.voice_states) > 0:
+      #  go = True
     if go:
       voice = discord.utils.get(interaction.guild.voice_channels, name = channel.name)
       voice_client = discord.utils.get(client.voice_clients, guild = interaction.guild)
@@ -114,7 +118,7 @@ async def bruh(interaction: discord.Interaction, length: int, secret: bool = Fal
     await reportcommanderror(interaction, traceback.format_exc(), length=length, secret=secret)
 
 
-@tree.command(name = "msg_anon", description = "send a message anonomously", guild = discord.Object(TheGroup_id))
+@tree.command(name = "msg_anon", description = "send a message anonomously", guilds = server_objects)
 async def bruh(interaction: discord.Interaction, msg: str):
   try:
     await interaction.response.send_message("you can dismiss this :)", ephemeral = True)
@@ -122,7 +126,7 @@ async def bruh(interaction: discord.Interaction, msg: str):
   except:
     await reportcommanderror(interaction, traceback.format_exc(), msg=msg)
 
-@tree.command(name = "translate", description = "translate using google translate", guild = discord.Object(TheGroup_id))
+@tree.command(name = "translate", description = "translate using google translate", guilds = server_objects)
 async def translate(interaction: discord.Interaction, text : str, langfrom : str = "auto", langto : str = "en", hidden : bool = False):
   try:
     if langfrom not in googletrans.LANGCODES and langfrom not in googletrans.LANGUAGES and langfrom != "auto":
@@ -136,7 +140,7 @@ async def translate(interaction: discord.Interaction, text : str, langfrom : str
   except:
     await reportcommanderror(interaction, traceback.format_exc(), text = text, langfrom = langfrom, langto = langto, hidden = hidden)
 
-#@tree.command(name = "name", description = "description", guild = discord.Object(TheGroup_id))
+#@tree.command(name = "name", description = "description", guilds = server_objects)
 #async def name(interaction: discord.Interaction, *args):
 #  try:
 #    await interaction.response.send_message("message", ephemeral = True)
@@ -154,13 +158,21 @@ async def on_message(message: discord.Message):
   try:
     if message.author.id != client.user.id:
       if message.guild == None:
-        await client.get_channel(bruhChannel_id).send(message.content)
+        if message.author.id == Harvaria_id and message.content == "SYNC":
+          print("syncing...")
+          await client.get_user(Harvaria_id).send("syncing...")
+          for server_object in server_objects:
+            await tree.sync(guild = server_object)
+          await client.get_user(Harvaria_id).send("synced!")
+          print("synced!")
+        else:
+          await client.get_channel(bruhChannel_id).send(message.content)
       else:
         if message.author.bot and message.author.name == "MEE6" and "GG" in message.content and "You've wasted a lot of your life!" in message.content:
           print(message.mentions[0].name + " leveled up!")
           responses = ["very impressive", "most impressive", "waste of space", "wow", "nice", "very cool", "pathetic", "0/10, don't recommend"]
           await message.channel.send(f"{message.mentions[0].mention}, {r.choice(responses)}")
-        elif len(message.mentions) > 0:
+        elif len(message.mentions) > 0 and message.reference is None and not message.author.bot:
           msg: str = ""
           for mtn in message.mentions:
             msg += mtn.mention
@@ -170,7 +182,11 @@ async def on_message(message: discord.Message):
       #print(message.content)
       #await message.channel.send(message.content)
   except:
-    errormessage = f"Error occured parsing message from {message.author.mention} in {message.channel.mention}:\n{message.content}\n\n{traceback.format_exc()}"
+    try:
+      channel = message.channel.mention
+    except:
+      channel = "this DM"
+    errormessage = f"Error occured parsing message from {message.author.mention} in {channel}:\n{message.content}\n\n{traceback.format_exc()}"
     await client.get_user(Harvaria_id).send(errormessage)
 
 client.run(TOKEN)
